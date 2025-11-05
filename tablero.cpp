47ae24b6e8290ff3f5abae70a8ee5f9ac8e259a9
buitraaa <buitradaniel.f@gmail.com>
2025-11-05 16:09:38 -0500

    Implement board initialization and display functions
    Implementación de las funciones del tablero

---
+++ tablero.cpp
@@ -0,0 +1,128 @@
+#include "tablero.h"
+#include <iostream>
+using namespace std;
+
+void inicializarTablero(int tablero[tamanoTablero][tamanoTablero]){
+  for(int i=0;i<tamanoTablero;i++){
+      for(int j=0;j<tamanoTablero;i++){
+          tablero[i][j] = vacia;
+      }
+  }
+
+  for(int i=0;i<4;i++){
+      for(int j=0;j<tamanoTablero;j++){
+        if(casillaJugable(i, j )){
+          tablero[i][j] = negra;
+        }
+      }
+  }
+
+  for(int i=6;i<tamanoTablero;i++){
+      for(int j=0;j<tamanoTablero;i++){
+        if(casillaJugable(i,j)){
+          tablero[i][j] = blanca;
+        }
+      }
+  }
+
+
+void mostrarTablero(int tablero[tamanoTablero][tamanoTablero]){
+  cout << endl << "\n   ";
+
+  for(char letra = 'A'; letra <= 'J';letra++){
+      cout << " " << letra << " ";
+  }
+  cout << "\n";
+
+  for(int i=0;i<tamanoTablero;i++){
+  if(i + 1 <10){
+    cout << " " << (i + 1) << " ";
+    }else {
+        cout << (i+1) << " ";
+  }
+
+  for(int j=0;j<tamanoTablero;j++){
+      int pieza = tablero[i][j];
+
+      if(!casillaJugable(i, j)){
+        cout << " . ";
+      } else {
+          switch (pieza) {
+            case vacia:
+              cout << " # ";
+              break;
+            case blanca:
+              cout << " B ";
+              break;
+             case negra:
+              cout << " N ";
+              break;
+             case damaBlanca:
+              cout << " W ";
+              break;
+             case damaNegra:
+              cout << " Q ";
+              break;
+            default:
+              cout << "Error"<< endl;
+                }
+            }
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+int obtenerPosPieza(int tablero[tamanoTablero][tamanoTablero], int fila, int columna){
+    if(!casillaJugable(fila,columna)){
+      return -1;
+    }
+    return tablero[fila][columna];
+}
+
+
+void colocarPieza(int tablero[tamanoTablero][tamanoTablero], int fila, int columna, int pieza) {
+    if (casillaValida(fila, columna)) {
+        tablero[fila][columna] = pieza;
+    }
+}
+
+
+bool casillaValida(int fila, int columna) {
+    return (fila >= 0 && fila < tamanoTablero && columna >= 0 && columna < tamanoTablero);
+}
+
+
+
+bool esCasillaOscura(int fila, int columna) {
+    return (fila + columna) % 2 != 0;
+}
+
+
+
+int contarPiezas(int tablero[tamanoTablero][tamanoTablero], int color) {
+    int contador = 0;
+    
+    for (int i = 0; i < tamanoTablero; i++) {
+        for (int j = 0; j < tamanoTablero; j++) {
+            int pieza = tablero[i][j];
+            
+            
+            if (color == blanca) {
+               
+                if (pieza == blanca || pieza == damaBlanca) {
+                    contador++;
+                }
+            } else if (color == negra) {
+                if (pieza == negra || pieza == damaNegra) {
+                    contador++;
+                }
+            }
+        }
+    }
+    
+    return contador;
+}
+
+
+
