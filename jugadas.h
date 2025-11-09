828be29e9d22c80f99950a199126bfe2c7f57a9b
buitraaa <buitradaniel.f@gmail.com>
2025-11-09 11:28:31 -0500

    Change 'Posicion' to 'posicion' in jugadas.h

--- jugadas.h
+++ jugadas.h
@@ -4,15 +4,15 @@
 #include "tablero.h"
 
 
-bool esMovimientoSimpleValido(int tablero[tamanoTablero][tamanoTablero], Posicion origen, Posicion destino);
-bool esCapturaValida(int tablero[tamanoTablero][tamanoTablero], Posicion origen, Posicion destino, Posicion &piezaCapturada);
+bool esMovimientoSimpleValido(int tablero[tamanoTablero][tamanoTablero], posicion origen, posicion destino);
+bool esCapturaValida(int tablero[tamanoTablero][tamanoTablero], posicion origen, posicion destino, posicion &piezaCapturada);
 bool puedeCapturarMas(int tablero[tamanoTablero][tamanoTablero], int fila, int columna);
 bool sonEnemigas(int pieza1, int pieza2);
 int obtenerColor(int pieza);
 bool esDama(int pieza);
 
-void realizarMovimientoSimple(int tablero[tamanoTablero][tamanoTablero], Posicion origen, Posicion destino);
-void realizarCaptura(int tablero[tamanoTablero][tamanoTablero], Posicion origen, Posicion destino, Posicion piezaCapturada);
+void realizarMovimientoSimple(int tablero[tamanoTablero][tamanoTablero], posicion origen, posicion destino);
+void realizarCaptura(int tablero[tamanoTablero][tamanoTablero], posicion origen, posicion destino, posicion piezaCapturada);
 void verificarPromocion(int tablero[tamanoTablero][tamanoTablero], int fila, int columna);
 
 #endif
